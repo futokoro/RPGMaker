fb139a734b690a958313afcf92bc9d6267df9a72
futokoro <hutokoroyj@yahoo.co.jp>
2017-04-07 11:23:59 +0900

    upload

--- FTKR_ExEscapeCharacters.js
+++ FTKR_ExEscapeCharacters.js
@@ -3,8 +3,8 @@
 // FTKR_ExEscapeCharacters.js
 // 作成者     : フトコロ
 // 作成日     : 2017/03/27
-// 最終更新日 : 
-// バージョン : v1.0.0
+// 最終更新日 : 2017/03/28
+// バージョン : v1.0.2
 //=======↑本プラグインを改変した場合でも、この欄は消さないでください↑===============
 
 var Imported = Imported || {};
@@ -14,7 +14,7 @@ var FTKR = FTKR || {};
 FTKR.EEC = FTKR.EEC || {};
 
 /*:
- * @plugindesc v1.0.0 制御文字を拡張するプラグイン
+ * @plugindesc v1.0.2 制御文字を拡張するプラグイン
  * @author フトコロ
  *
  * @param -- 画像の表示 --
@@ -24,6 +24,11 @@ FTKR.EEC = FTKR.EEC || {};
  * @desc ゲーム起動時に読み込む画像ファイル名を指定
  * 複数ある場合は、カンマ(,)で区切ること
  * @default 
+ *
+ * @param Enabled Adjust Height
+ * @desc 画像高さに合わせて、行の高さを自動調整する
+ * 0 - 無効にする, 1 - 有効にする
+ * @default 0
  *
  * @param Image Width
  * @desc 制御文字\IMG[]で表示する画像の幅
@@ -40,8 +45,10 @@ FTKR.EEC = FTKR.EEC || {};
  * 文章の表示等に使用できる制御文字に、以下の制御文字を追加します。
  * 
  * 1. 画像の表示
- * \IMG[画像ファイル名(, 画像番号, 画像幅, 画像高さ, 画像拡大率)]
+ * \IMG[画像ファイル名(, 画像番号, 画像幅, 画像高さ, 画像拡大率, 表示位置)]
  * 
+ * 2. 行の高さの変更
+ * \LH[行の高さ]
  * 
  *-----------------------------------------------------------------------------
  * 設定方法
@@ -55,7 +62,7 @@ FTKR.EEC = FTKR.EEC || {};
  *-----------------------------------------------------------------------------
  * 以下の制御文字で画像を表示できます。
  * 
- * \IMG[画像ファイル名(, 画像番号, 画像幅, 画像高さ, 画像拡大率)]
+ * \IMG[画像ファイル名(, 画像番号, 画像幅, 画像高さ, 画像拡大率, 表示位置)]
  * 
  * 画像ファイル名
  *    :表示させたい画像のファイル名を指定します。
@@ -76,25 +83,50 @@ FTKR.EEC = FTKR.EEC || {};
  *    :画像の高さを指定します。
  *    :0を指定、または入力しない場合は、プラグインパラメータ<Image Height>の値を
  *    :使用します。
+ *    :プラグインパラメータ<Enabled Adjust Height>を有効にした場合、
+ *    :画像の高さに合わせて以降の行の高さを自動で調整します。
  * 
  * 画像拡大率
  *    :100を基準として、画像の拡大率を指定します。
  *    :入力しない場合は、元のサイズのまま表示します。
+ * 
+ * 表示位置
+ *    :行の高さよりも、画像高さが小さい場合に、画像を表示する位置を設定できます。
+ *    : 中央(middle)   - 中央揃えで表示
+ *    : 下揃え(bottom) - 下揃えで表示
+ *    :入力しない場合は、上揃えで表示します。
  * 
  * 入力例)
  *    \IMG[file]
  *      画像ファイル file.png の 0番目の画像を表示する。
  *      画像の幅と高さは、プラグインパラメータの値を使用する。
  * 
- *    \IMG[file, 1, 48, 48, 120]
- *      48*48 の画像の画像ファイル file.png の 1番目の画像を120%に
- *      拡大して表示する。
+ *    \IMG[file, 1, 48, 48, 80, 中央]
+ *      48*48 の画像の画像ファイル file.png の 1番目の画像を80%に
+ *      縮小して表示する。
+ *      このとき、1行が48よりも大きい場合に中央揃えで表示する。
  * 
  * (*1):画像ファイルは、プラグインパラメータ<Advance Load File>に
  *      ファイル名を入力することで、ゲーム起動時に事前に読み込むことが
  *      できます。
  *      事前に読み込んでいない画像の場合、ゲーム起動後の最初の画像表示の際に
  *      正しく表示できない場合があります。
+ * 
+ * 
+ *-----------------------------------------------------------------------------
+ * 「行の高さの変更」機能
+ *-----------------------------------------------------------------------------
+ * 以下の制御文字で行の高さの変更できます。
+ * 
+ * \LH[行の高さ]
+ * 
+ * 行の高さ
+ *    :この制御文字以降の1行の高さを、pixel単位で指定します。
+ *    :MV標準の行の高さは 36 です。
+ *    :0 を指定すると、標準の高さに変更します。
+ * 
+ * 入力例)
+ *    \LH[48]
  * 
  * 
  *-----------------------------------------------------------------------------
@@ -119,6 +151,12 @@ FTKR.EEC = FTKR.EEC || {};
  * 変更来歴
  *-----------------------------------------------------------------------------
  * 
+ * v1.0.2 - 2017/03/28 : 不具合修正
+ * 
+ * v1.0.1 - 2017/03/27 : 機能追加
+ *    1. 制御文字「行の高さの変更」を追加
+ *    2. 制御文字「画像の表示」に、行の高さと表示位置の調整機能を追加
+ * 
  * v1.0.0 - 2017/03/27 : 初版作成
  * 
  *-----------------------------------------------------------------------------
@@ -135,6 +173,7 @@ FTKR.EEC.parameters = PluginManager.parameters('FTKR_ExEscapeCharacters');
 FTKR.EEC.files = String(FTKR.EEC.parameters['Advance Load Files'] || '');
 FTKR.EEC.width = Number(FTKR.EEC.parameters['Image Width'] || 0);
 FTKR.EEC.height = Number(FTKR.EEC.parameters['Image Height'] || 0);
+FTKR.EEC.enabledAdjustHeight = Number(FTKR.EEC.parameters['Enabled Adjust Height'] || 0);
 
 //画像ファイル内で画像を横に並べられる数
 Window_Base.EEC_IMG_LENGTH = 16;
@@ -183,6 +222,9 @@ Window_Base.prototype.processEscapeCharacter = function(code, textState) {
     case 'IMG':
         this.processDrawEecImg(this.obtainEscapeEecParam(textState), textState);
         break;
+    case 'LH':
+        textState.height = this.obtainEscapeParam(textState) || this.lineHeight();
+        break;
     default:
         FTKR.EEC.Window_Base_processEscapeCharacter.call(this, code, textState);
         break;
@@ -195,7 +237,7 @@ Window_Base.prototype.obtainEscapeEecParam = function(textState) {
         textState.index += arr[0].length;
         var eec = arr[1].split(',');
         eec = eec.map( function(elm, i) {
-            return i > 0 ? parseInt(elm) : elm;
+            return isNaN(parseInt(elm)) ? elm : parseInt(elm);
         });
         return eec;
     } else {
@@ -208,6 +250,25 @@ Window_Base.prototype.processDrawEecImg = function(ececs, textState) {
     var width = ececs[2] || FTKR.EEC.width;
     var height = ececs[3] || FTKR.EEC.height;
     var scale = ececs[4] / 100 || 1;
-    this.drawEecImage(ececs[0], index, textState.x + 2, textState.y + 2, width, height, scale);
+    var imgHeight = 0;
+    var offset = 0; 
+    if(FTKR.EEC.enabledAdjustHeight) {
+        imgHeight = Math.floor(height * scale) + 4;
+        var offset = 0; 
+        switch (ececs[5]) {
+            case 'middle':
+            case '中央':
+                offset = (textState.height - imgHeight) / 2;
+                if(offset < 0) offset = 0;
+                break;
+            case 'bottom':
+            case '下揃え':
+                offset *= 2;
+                break;
+        }
+    }
+    this.drawEecImage(ececs[0], index, textState.x + 2, textState.y + 2 + offset, width, height, scale);
     textState.x += Math.floor(width * scale) + 4;
+    textState.height = Math.max(imgHeight, textState.height);
 };
+
